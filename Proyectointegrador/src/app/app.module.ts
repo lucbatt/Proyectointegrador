8c79900ea6994649b7ddc70aec39f66bd0d5cdaa
Analía <variedadesxxi@hotmail.com>
2023-01-13 23:30:04 -0300

    update app.modulets -se agrego forms module

--- Proyectointegrador/src/app/app.module.ts
+++ Proyectointegrador/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -33,7 +33,8 @@ import { FooterComponent } from './components/footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgCircleProgressModule.forRoot({})
+    NgCircleProgressModule.forRoot({}),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
